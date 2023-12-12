adcc4824b41c15e5848b5de92638c97f399dd2c5
Kamal Rachakonda <kamal.racha1107@gmail.com>
2023-12-12 12:44:30 +0000

    hi

--- models/Learning/store_sales_profit_2023.sql
+++ models/Learning/store_sales_profit_2023.sql
@@ -1,20 +1,3 @@
 
-with store_final as
-(
-select *
-from {{ ref('store_tbl_2023') }}
-),
-store_sales_final as
-(select *
-from {{ ref('store_sales_tbl_2023') }}
-),
-
- final_sales_profit as
-(
-    select s_store_name,s_county sum(SS_SALES_PRICE),sum(ss_net_profit) from store_final,store_sales_final
-where S_STORE_SK=SS_STORE_SK
-group by 1,2
-)
-select * from final_sales_profit
 
 
