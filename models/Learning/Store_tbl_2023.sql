72c511abd5f8492b1660e29a47cfb64712e9b1d5
Kamal Rachakonda <kamal.racha1107@gmail.com>
2023-12-12 13:59:32 +0000

    hello

--- models/Learning/Store_tbl_2023.sql
+++ models/Learning/Store_tbl_2023.sql
@@ -1,5 +1,5 @@
-with Store_tbl_2023 as
+with Store_tbll_2023 as
 (
 select S_STORE_SK,S_STORE_ID,s_store_name,s_country,s_county from NEWSCH.STORE_2023
 )
-select * from Store_tbl_2023
+select * from Store_tbll_2023
