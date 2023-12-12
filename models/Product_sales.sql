7eda1d3ee465f410ab7a93bbb13ce747d7252980
Kamal Rachakonda <kamal.racha1107@gmail.com>
2023-12-12 09:48:33 +0000

    hi

--- models/Product_sales.sql
+++ models/Product_sales.sql
@@ -1,5 +1,5 @@
 with product_sales as (
-select city,state,sum(NA_SALES)sum(GLOBAL_SALES) from product
+select city,state,sum(NA_SALES),sum(GLOBAL_SALES) from product
 group by 1,2 
 )
 Select * from product_sales
