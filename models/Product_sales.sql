a330994e7b55436091646d4b1ba95d5d67c88c37
Kamal Rachakonda <kamal.racha1107@gmail.com>
2023-12-12 10:31:35 +0000

    table creation

--- models/Product_sales.sql
+++ models/Product_sales.sql
@@ -1,6 +1,11 @@
+
+{{ config(materialized='table') }}
+
 with product_sales as (
 select city,state,sum(NA_SALES),sum(GLOBAL_SALES) from NEWSCH.product
 group by 1,2 
 )
 Select * from product_sales
+
+
 
