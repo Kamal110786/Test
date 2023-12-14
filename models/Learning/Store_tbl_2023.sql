9422fa88e2289f792469e19b2af58d284aad71ec
Kamal Rachakonda <kamal.racha1107@gmail.com>
2023-12-14 11:12:46 +0000

    Source yml files

--- models/Learning/Store_tbl_2023.sql
+++ models/Learning/Store_tbl_2023.sql
@@ -2,6 +2,6 @@
 
 with Store_tbl_2023 as
 (
-select S_STORE_SK,S_STORE_ID,s_store_name,s_country,s_county from NEWSCH.store_2023
+select S_STORE_SK,S_STORE_ID,s_store_name,s_country,s_county from {{ source('test_NEWSCH', 'STORE_2023')}}
 )
 select * from Store_tbl_2023
