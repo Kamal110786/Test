fa04a190789621d72267239b644e04877388c023
Kamal Rachakonda <kamal.racha1107@gmail.com>
2023-12-14 10:25:21 +0000

    Python1

---
+++ models/Learning/Pythontransform.py
@@ -0,0 +1,18 @@
+import snowflake.snowpark.functions as F
+def model(dbt,session):
+    
+    dbt.config(materialized="table")
+    Store_=dbt.ref("Store_tbl_2023")
+    Store_Sales_=dbt.ref("store_sales_tbl_2023")
+    
+    final_df = (
+        Store_
+        .join(Store_Sales_, Store_Sales_.SS_STORE_SK == Store_.S_STORE_SK, 'Inner')
+        .group_by(Store_.S_STORE_NAME,Store_.S_COUNTY)
+            .agg(
+            F.sum(F.col('SS_SALES_PRICE')).alias('SALES_PRICE')
+        
+        )
+    )
+
+    return final_df
